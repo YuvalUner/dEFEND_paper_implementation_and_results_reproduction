Made model saving function save models in subdirectory of checkpoints dir, fixed errors with defend_no_comments.py model, fixed issue where co-attention layer got arguments in the wrong order.

model/co_attention_layer.py
@@ -26,8 +26,8 @@ class CoAttentionLayer(nn.Module):
         return mask
 
     def forward(self, x, return_attention=False):
-        comment_rep = x[0]
-        sentence_rep = x[1]
+        sentence_rep = x[0]
+        comment_rep = x[1]
 
         comment_rep_transpose = comment_rep.transpose(-2, -1)
         sentence_rep_transpose = sentence_rep.transpose(-2, -1)

model/dEFEND.py
@@ -39,6 +39,7 @@ class Defend(nn.Module):
         :param opt: The options for the model
         """
         super(Defend, self).__init__()
+        print(f"Model: {self.__class__.__name__}")
         self.opt = opt
         encoding_dim = 2 * opt.d if opt.bidirectional else opt.d
         embedding_index = {"<padding/>": torch.zeros(opt.embedding_dim)}
@@ -288,12 +289,12 @@ class Defend(nn.Module):
         metrics['loss'] = np.mean(loss_vals)
 
         # Save the model if the epoch is a multiple of the save_epoch_freq
-        if epoch % self.opt.save_epoch_freq == 0:
+        if (epoch + 1) % self.opt.save_epoch_freq == 0:
             # Check if the checkpoints directory exists. If not, create it
             import os
-            if not os.path.exists(self.opt.checkpoints_dir):
-                os.makedirs(self.opt.checkpoints_dir)
-            torch.save(self.state_dict(), f'{self.opt.checkpoints_dir}/{self.opt.name}_{epoch}.pt')
+            if not os.path.exists(self.opt.checkpoints_dir + "/" + self.opt.name):
+                os.makedirs(self.opt.checkpoints_dir + "/" + self.opt.name)
+            torch.save(self.state_dict(), f'{self.opt.checkpoints_dir}/{self.opt.name}/{self.opt.name}_{epoch + 1}.pt')
 
         return metrics
 

model/defend_no_comments.py
@@ -14,7 +14,6 @@ class DefendNoComments(Defend):
     def forward(self, comments, articles, return_attention=False):
 
         articles = self.article_embedding(articles)
-        comments = torch.eye(articles.size(1)).to(articles.device).index_select(0, comments)
 
         article_sentence_encodings = torch.zeros((articles.shape[0], self.opt.max_sentence_count,
                                                   2 * self.opt.d if self.opt.bidirectional else self.opt.d))
@@ -24,15 +23,16 @@ class DefendNoComments(Defend):
             article_sentence_embedding, _ = self.sentence_encoder(article_word_embedding)
             article_sentence_encodings[i] = article_sentence_embedding
 
-        article_comments_encodings = torch.zeros((comments.shape[0], self.opt.max_comment_count,
-                                                  2 * self.opt.d if self.opt.bidirectional else self.opt.d))
-
+        # Create a tensor of shape (batch_size, max_comment_count, embedding_dim), that would not affect the output
+        comments = torch.eye(self.opt.max_comment_count, self.opt.d * 2 if self.opt.bidirectional else self.opt.d)
+        comments = comments.unsqueeze(0).repeat(articles.shape[0], 1, 1)
+        comments = comments.to(article_sentence_encodings.device)
 
         if return_attention:
-            co_attention_output, As, Ac = self.co_attention((article_sentence_encodings, article_comments_encodings), return_attention=True)
+            co_attention_output, As, Ac = self.co_attention((article_sentence_encodings, comments), return_attention=True)
             return self.fc(co_attention_output), As, Ac
         else:
-            co_attention_output = self.co_attention((article_sentence_encodings, article_comments_encodings))
+            co_attention_output = self.co_attention((article_sentence_encodings, comments))
             # Feed the co-attention to the fully connected layer
             output = self.fc(co_attention_output)
             return output